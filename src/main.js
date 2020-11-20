e3d94f55bb40bc4e7e1c2e613603c22413b73716
Ellie Lambert <ebezjian@gmail.com>
2020-11-20 15:59:11 -0500

    fix of api not working in process.env.api

--- src/main.js
+++ src/main.js
@@ -15,31 +15,10 @@ $(document).ready(function () {
   $("#convertButton").click(function () {
     let usBase = $("#usCurrency").val();
     clearFields();
-    let promise = new Promise(function(resolve, reject) {
-    const value = Math.floor(Math.random() * Math.floor(2))
-    if (value === 1) {
-      resolve("the value is 1");
-    } else {
-      reject("the value is 0");
-    }
     console.log(Promise)
     CurrencyExchange.exchange()
       .then((exchangeRes) => {
         console.log(exchangeRes)
       })
-      //  (response) {
-      //   console.log(response)
-      //   const body = JSON.parse(response);
-      //   $(".showCurrency1").text(
-      //     `This is how much you would have in Korea is ${body.conversion_rates.KRW} from ${usBase}`
-      //   );
-      //   $(".showErrors").text("");
-      // },
-      function (error) {
-        $(".showErrors").text(
-          `There was an error processing your request:${error}`
-        );
-      }
-    );
   });
 });
--- src/second.js
+++ src/second.js
@@ -2,10 +2,11 @@ export default class CurrencyExchange {
   static async exchange() {
     console.log(process.env.API_KEY)
       fetch(
-      `https://v6.exchangerate-api.com/v6/0b1cb3dd097b644cbf0a7166/latest/USD`
+      `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
     )
     
       .then((response) => {
+        console.log(response)
         if (!response.ok) {
           throw Error(response.message);
         }
