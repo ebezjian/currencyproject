519297ac83f06144d39bb2c0300cd5de135d8881
Ellie Lambert <ebezjian@gmail.com>
2020-11-20 12:49:18 -0500

    add basic api function to main.js, deciding to use select options for html

--- src/main.js
+++ src/main.js
@@ -5,8 +5,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
-// $(document).ready(function() {
-//   $('TODO').submit(function(event) {
-//     event.preventDefault();
-//   });
-// });
+$('#convertButton').click(function() {
+  const baseCurrency = $('#usCurrency').val();
+  $('#usCurrency').val("");
+
+  let request = new XMLHttpRequest();
+  const url = `https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`;
+
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  }
+  function getElements(response) {
+    $('.showCurrency1').text(`This is how much you would have in Korea is ${response.}`)
+  }
+})
