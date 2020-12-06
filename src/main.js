80733220d823c3bce4ce315426c9a3c87465aa42
Ellie Lambert <ebezjian@gmail.com>
2020-12-05 19:54:01 -0500

    add new function to main.js to make sure actual conversion happens and to show errors

--- src/main.js
+++ src/main.js
@@ -5,28 +5,55 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/styles.css";
 import CurrencyExchange from "./second.js";
 
-$(document).ready(function () {
-  CurrencyExchange.exchange().then((exchangeRes) => {
-    if (exchangeRes.conversion_rates) {
-      const rates = exchangeRes.conversion_rates;
-      if (isNaN(exchangeRes.conversion_rates)){
-        $('#showErrors').text('there was an error: this currency is not available');
-      }
+// $(document).ready(function () {
+//   CurrencyExchange.exchange().then((exchangeRes) => {
+//     if (exchangeRes.conversion_rates) {
+//       const rates = exchangeRes.conversion_rates;
+//       if (isNaN(exchangeRes.conversion_rates)){
+//         $('#showErrors').text('there was an error: this currency is not available');
+//       }
 
       
-      $.each(rates, function (key, value) {
-        $("#otherCountry").append(
-          $("<option></option>").attr("value", value).text(key)
-        );
-      });
-    } else {
-      $(".showErrors").text(`There was an error:${exchangeRes.message}`);
-    }
-  });
+//       $.each(rates, function (key, value) {
+//         $("#otherCountry").append(
+//           $("<option></option>").attr("value", value).text(key)
+//         );
+//       });
+//     } else {
+//       $(".showErrors").text(`There was an error:${exchangeRes.message}`);
+//     }
+//   });
 
-  $("#convertButton").click(function () {
-    const finalValue = $("select#otherCountry").val();
+//   $("#convertButton").click(function () {
+//     const finalValue = $("select#otherCountry").val();
 
-    return $("#result").text(finalValue);
-  });
-});
+//     return $("#result").text(finalValue);
+//   });
+// });
+
+function showingErrors(error) {
+  $('.showErrors').text(`${error}`);
+}
+
+$(document).ready(function(){
+  $('#convertButton').click(function(){
+    const murica = $('#USD').val();
+    const currency = $('#otherCountry :selected').val();
+
+    CurrencyExchange.exchange()
+    .then(function(response) {
+      if (response instanceof Error || currency !== currency) {
+        throw Error (`Woops : ${response.messsage}`);
+      }
+
+      const entry = response;
+      const rates = entry.conversion_rates[currency];
+      const ratesMath = rate * murica;
+      const totalConversion = ratesMath.toFixed(2); //to delete all them pesky decimal spots
+      $('#result').text(`You asked to convert those dolla dolla bills at ${rates} and now you've got ${totalConversion}`)
+    })
+    .catch(function(error) {
+      showingErrors(error.message);
+    })
+  })
+})
